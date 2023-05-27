152765c2ef659a4bf37ef61ead4ed128bfa6d2fd
John Doe <johndoe@example.com>
2023-05-27 14:20:22 +0200

    iteration 1 and 2

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+function Greeting(props) {
+  if (props.lang === "de") {
+    return <div class="box">Ludwig {props.name}</div>
+  } 
+  if (props.lang === "fr") {
+    return <div class="box">François {props.name}</div>
+  } 
+  if (props.lang === "es") {
+    return <div class="box">Hola {props.name}</div>
+  } 
+  else {
+    return ( <div class="box">Hello {props.name}</div>
+    );
+  } 
+}
+export default Greeting;
+
+
